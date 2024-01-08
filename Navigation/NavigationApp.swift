4cb704c14b9b4e73c3c43a46ca436247e432bd0d
tomohiro1024 <gongyezhihong732@gmail.com>
2024-01-08 12:12:33 +0900

    first commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by 宮野智宏 on 2024/01/08.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
