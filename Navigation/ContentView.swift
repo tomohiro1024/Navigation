21a762c87511e8d70ad25b957cefdb7af10cd506
tomohiro1024 <gongyezhihong732@gmail.com>
2024-01-09 18:21:24 +0900

    I learned about @Binding

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,22 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     @State var num = 0
+    @State var isShow = false
     
     var body: some View {
         NavigationStack {
-            HStack {
-                Button("-") {
-                    num = num - 1
+            VStack {
+                HStack {
+                    Button("-") {
+                        num -= 1
+                        
+                    }
+                    Text("Counter: \(num)")
+                    Button("+") {
+                        num += 1
+                    }
+
+                }
+                .padding()
+                Button("画面遷移") {
+                    isShow = true
                     
                 }
-                Text("Counter: \(num)")
-                Button("+") {
-                    num = num + 1
-                }
-
             }
-            .padding()
             .font(.title)
+            .sheet(isPresented: $isShow) {
+                MyView(count: $num)
+            }
+        }
+    }
+}
+
+struct MyView: View {
+    @Binding var count: Int
+    
+    var body: some View {
+        Button("+5") {
+            count += 5
+            
         }
     }
 }
